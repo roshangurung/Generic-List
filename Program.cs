13a2ed65fba5c01c548ae9f7cb74d83ddbdbc711
roshangurung <r.gurung1992@gmail.com>
2019-11-14 13:11:12 -0800

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generic_List
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = new List<int>() { 1, 2, 3, 4, 5 };
+            numbers.Add(1);
+
+            foreach (var n in numbers)
+                Console.WriteLine(n);
+        }
+    }
+}
+/////Output:{1,2,3,4,5,1}/////
